f8413cf6e297a79fe163256d65a0cc43d334b098
lukedevfull <contato.prof2@gmail.com>
2025-08-10 20:31:22 -0300

    Add application.properties with app name

---
+++ src/main/java/br/com/lukedevfull/RestWithSprongBootAndJavaLukeApplication.java
@@ -0,0 +1,13 @@
+package br.com.lukedevfull;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestWithSprongBootAndJavaLukeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestWithSprongBootAndJavaLukeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/lukedevfull/RestWithSprongBootAndJavaLukeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.lukedevfull;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestWithSprongBootAndJavaLukeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
