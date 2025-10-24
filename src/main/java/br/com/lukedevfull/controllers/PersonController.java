c08ba35ed4979f39271dfc7b3fc504e9e24b452e
lukedevfull <contato.prof2@gmail.com>
2025-10-24 13:45:12 -0300

    feat: implementa endpoint para buscar pessoa por ID
    - Adiciona endpoint GET /persons/{id} no PersonController
    - Implementa serviço PersonServices com método findByID
    - Adiciona tratamento básico de retorno de dados mockados
    - Configura injeção de dependência entre Controller e Service
    - Adiciona anotações necessárias para o Spring Boot

--- src/main/java/br/com/lukedevfull/controllers/PersonController.java
+++ src/main/java/br/com/lukedevfull/controllers/PersonController.java
@@ -1,8 +1,30 @@
 package br.com.lukedevfull.controllers;
 
 
+import br.com.lukedevfull.model.Person;
+import br.com.lukedevfull.services.PersonServices;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.awt.*;
+
 @RestController
+@RequestMapping("/persons")
 public class PersonController {
+
+    @Autowired
+    private PersonServices service;
+    // private PersonServices service = new PersonServices;
+
+    @RequestMapping(value = "/{id}",
+        method = RequestMethod.GET,
+        produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public Person findByID(@PathVariable("id") String id){
+        return service.findByID(id);
+    }
 }
--- src/main/java/br/com/lukedevfull/services/PersonServices.java
+++ src/main/java/br/com/lukedevfull/services/PersonServices.java
@@ -1,15 +1,28 @@
 package br.com.lukedevfull.services;
 
+import br.com.lukedevfull.model.Person;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.concurrent.atomic.AtomicLong;
+import java.util.logging.Logger;
 
 @Service
 public class PersonServices {
     private final AtomicLong counter = new AtomicLong();
 
-    @Autowired
-    private PersonServices person;
-        // private PersonServices person = new PersonServices;
+    private Logger logger = Logger.getLogger(PersonServices.class.getName());
+
+    public Person findByID(String id){
+        logger.info("Fiding one person.");
+
+        Person person = new Person();
+        person.setId(counter.incrementAndGet());
+        person.setFirstname("John");
+        person.setLastName("Doe");
+        person.setAddress("123 Main St");
+        person.setGender("Male");
+
+        return person;
+    }
 }
