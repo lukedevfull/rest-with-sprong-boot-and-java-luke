a2b102eca065df582fea388cb33a3f69f17dd9a5
lukedevfull <contato.prof2@gmail.com>
2025-11-04 14:11:04 -0300

    feat: Implement CRUD operations for Person entity
    - Created Person model with basic attributes
    - Implemented PersonServices with mock data operations
    - Added RESTful endpoints in PersonController for CRUD operations
    - Included proper HTTP methods and response types
    - Added basic logging for service operations

--- src/main/java/br/com/lukedevfull/controllers/PersonController.java
+++ src/main/java/br/com/lukedevfull/controllers/PersonController.java
@@ -5,12 +5,10 @@ import br.com.lukedevfull.model.Person;
 import br.com.lukedevfull.services.PersonServices;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.awt.*;
+import java.util.List;
 
 @RestController
 @RequestMapping("/persons")
@@ -20,11 +18,47 @@ public class PersonController {
     private PersonServices service;
     // private PersonServices service = new PersonServices;
 
+    //Find All
+    @RequestMapping(
+            method = RequestMethod.GET,
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public List<Person> findAll (){
+        return service.findAll();
+    }
+
+    //Find by ID
     @RequestMapping(value = "/{id}",
         method = RequestMethod.GET,
         produces = MediaType.APPLICATION_JSON_VALUE
     )
     public Person findByID(@PathVariable("id") String id){
         return service.findByID(id);
+    }
+    //Create
+    @RequestMapping(
+        method = RequestMethod.POST,
+        consumes = MediaType.APPLICATION_JSON_VALUE,
+        produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public Person create(@RequestBody Person person){
+        return service.create(person);
+    }
+    //UPDATE
+    @RequestMapping(
+        method = RequestMethod.PUT,
+        consumes = MediaType.APPLICATION_JSON_VALUE,
+        produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public Person update(@RequestBody Person person){
+        return service.update(person);
+    }
+
+    //DELETE
+    @RequestMapping(value = "/{id}",
+            method = RequestMethod.DELETE
+    )
+    public void delete(@PathVariable("id") String id){
+        service.delete(id);
     }
 }
--- src/main/java/br/com/lukedevfull/services/PersonServices.java
+++ src/main/java/br/com/lukedevfull/services/PersonServices.java
@@ -4,8 +4,11 @@ import br.com.lukedevfull.model.Person;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.logging.Logger;
+import java.util.stream.Collectors;
 
 @Service
 public class PersonServices {
@@ -13,9 +16,32 @@ public class PersonServices {
 
     private Logger logger = Logger.getLogger(PersonServices.class.getName());
 
+    public List<Person> findAll(){
+
+        List<Person> persons = new ArrayList<Person>();
+        //      Data MOCK ---->
+        for (int i = 0; i < 10; i++) {
+            Person person = mockPerson(i);
+            persons.add(person);
+        }
+        logger.info("Find All Persons.");
+        return persons;
+    }
+
+    private Person mockPerson(int i){
+        Person person = new Person();
+        person.setId(counter.incrementAndGet());
+        person.setFirstname("FIRTSNAME " + i);
+        person.setLastName("LASTNAME " + i);
+        person.setAddress("ADRESS " + i);
+        person.setGender("GENDER " + i);
+        return person;
+    }
+
     public Person findByID(String id){
         logger.info("Fiding one person.");
 
+        //      Data MOCK ---->
         Person person = new Person();
         person.setId(counter.incrementAndGet());
         person.setFirstname("John");
@@ -24,5 +50,21 @@ public class PersonServices {
         person.setGender("Male");
 
         return person;
+    }
+
+    public Person create(Person person){
+        logger.info("Creating one person.");
+
+        return person;
+    }
+
+    public Person update(Person person){
+        logger.info("Updating one person.");
+
+        return person;
+    }
+
+    public void delete(String id){
+        logger.info("Deleting one person.");
     }
 }
