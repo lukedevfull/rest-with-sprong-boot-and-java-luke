59d6131bd1a1ff4a17afc9211a085758841391d6
Luke <contato.prof2@gmail.com>
2026-02-04 23:27:30 -0300

    fix: rename and refactory the application fix: created and conect on a DB with Heidi feat: update password of the conection on DB

--- src/main/java/br/com/lukedevfull/RestWithSpringBootAndJavaLukeApplication.java
+++ src/main/java/br/com/lukedevfull/RestWithSpringBootAndJavaLukeApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RestWithSprongBootAndJavaLukeApplication {
+public class RestWithSpringBootAndJavaLukeApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(RestWithSprongBootAndJavaLukeApplication.class, args);
+		SpringApplication.run(RestWithSpringBootAndJavaLukeApplication.class, args);
 	}
 
 }
