34977302f45deec1d2f38e566de6aa3f99d33f8a
Luke <contato.prof2@gmail.com>
2026-06-18 20:38:18 -0300

    feat: terminei de implementar o repositorio

--- src/main/java/br/com/lukedevfull/repository/PersonRepository.java
+++ src/main/java/br/com/lukedevfull/repository/PersonRepository.java
@@ -1,4 +1,7 @@
 package br.com.lukedevfull.repository;
 
-public interface PersonRepository {
+import br.com.lukedevfull.model.Person;
+import org.springframework.data.jpa.repository.JpaRepository;
+//@Repository
+public interface PersonRepository extends JpaRepository<Person, Long> {
 }
